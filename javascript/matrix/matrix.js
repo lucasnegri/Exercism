187da7c61852a578dd380150e396192f4270b772
Lucas Negri <lucas_negriiii@hotmail.com>
2022-08-19 14:38:45 -0300

    Matrix Exercise Resolved

---
+++ javascript/matrix/matrix.js
@@ -0,0 +1,20 @@
+//
+// This is only a SKELETON file for the 'Matrix' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export class Matrix {
+  constructor(matrix) {
+    this.string = matrix;
+  }
+
+  get rows() {
+    return this.string.split("\n").map((row) => row.split(' ').map(Number))
+  }
+
+  get columns() {
+    return this.rows[0].map((_,i) => this.rows.map(r => r[i]))
+  }
+}
+
+git 
